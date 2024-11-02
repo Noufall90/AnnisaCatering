2198df413099f4a8c935f10f2b032968a2753fc8
Noval ian <andrianau26@gmail.com>
2024-11-02 21:12:31 +0800

    anisa

---
+++ annisa-catering/src/App.vue
@@ -0,0 +1,18 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app">
+    <AnnisaCatering />
+  </div>
+</template>
+
+<script>
+// src/App.vue
+import AnnisaCatering from './components/AnnisaCatering.vue';
+
+
+export default {
+  components: {
+    AnnisaCatering,
+  },
+};
+</script>
